4dd76d2a93d496ff3189433930d615d66477ecc7
degarem <shamankingmaybe@gmail.com>
2021-05-07 00:03:54 +0300

    courseProject

---
+++ main.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: true,
+    },
+    speed: 1000,
+    direction: 'horizontal',
+    navigation:
+    {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination:
+    {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+    zoom: true,
+    keyboard:
+    {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    mousewheel:
+    {
+        invert: true,
+    },
+    autoplay:
+    {
+        delay: 5000,
+    },
+    loop: true,
+
+});
